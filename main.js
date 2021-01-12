efcf103d093aad4429e67606c69692b7fce7616e
enisaras <arass@rpi.edu>
2021-01-11 23:20:04 -0500

    Added required node files and some driver code to get started.

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 //main.js
+const Discord = require('discord.js');
+const {prefix, token} = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(token);
+//TODO:Later add the commands to their specific files
+client.on('message', message => {
+if(message.content === `${prefix}bet`){
+    message.channel.send('Command is coming soon!');
+}else if(message.content === `${prefix}view`)
+{
+    message.channel.send('Command is coming soon!');
+}
+});
