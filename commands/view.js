bed7ff0fcb229b90f2fac514fc6e219e6ba52689
enisaras <arass@rpi.edu>
2021-01-14 13:35:14 -0500

    Minor changes

--- commands/view.js
+++ commands/view.js
@@ -2,6 +2,12 @@
 This command will show the user what bets they can make and take an optional parameter
 "team name" to show a bet from a specific team.
 */
+let teams = ['Los Angeles Rams', 'Green Bay Packers', 'Baltimore Ravens', 'Buffalo Bills'];
+let matchups = [`${teams[0]} v. ${teams[1]}`, `${teams[2]} v. ${teams[3]}`];
+let odds = {
+	'money line' : +255,
+	'point spread' : -313,
+}
 const {prefix} = require('../config.json');
 module.exports = {
 	name: 'view',
