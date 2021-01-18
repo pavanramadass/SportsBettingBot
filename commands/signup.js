86656262022e1d2fafdf8e5f746db1cbc5db016b
Pavankumar <pavan.ramadassv@gmail.com>
2021-01-17 20:07:09 -0500

    Update signup.js
    Added a function that creates a new user in the database.

--- commands/signup.js
+++ commands/signup.js
@@ -1,6 +1,14 @@
 const { prefix } = require('../config.json');
+const { Users } = require ('../dbObjects.js'); 
 
-var player_ID = Math.floor((Math.random() * 100) + 1); 
+let player_ID = Math.floor((Math.random() * 10000) + 1); 
+
+// creates a new user in the database 
+function create(firstname, lastname, player_ID) {
+  const amount = 1000; 
+  const newUser = Users.create({ user_id: player_ID, first_name: firstname, last_name: lastname, balance: amount}); 
+  return newUser; 
+}
 
 module.exports = {
   name: 'signup',
@@ -10,6 +18,7 @@ module.exports = {
   execute(message, args) {
     let firstname = args[0];
     let lastname = args[1];
+    create(firstname, lastname, player_ID); 
     message.reply(`Thank you, ${firstname} for signing up! You have been given a starting balance of $1,000. Your player ID has been sent to your DM.`);
     message.author.send(`Your player ID is ${player_ID}. If you wish to delete your account, then use the player ID to delete the account.`); 
   },
